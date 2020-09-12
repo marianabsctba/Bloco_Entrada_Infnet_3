6947a5e96e3d8ee19bbd5e14907ea45f7345f18c
marianabsctba <62853909+marianabsctba@users.noreply.github.com>
2020-09-12 13:22:48 -0300

    Update Questão03_TP4.py

--- Questão03_TP4.py
+++ Questão03_TP4.py
@@ -1,15 +1,13 @@
-#Questão 03 do TP4
-#essa foi a questão que eu mais modifiquei.
-
 import pandas as pd
 
 def lin():
     print("--" * 30)
 
 def listar_contatos():
-    clientes = ("clientes_geral.csv")
     clientes = {"Nome": ["Ana", "Pedro", "Laura", "Marcos"], "Saldo": [1000.00, -500.00, -20.00, -2599.00], "Negativado": ["não", "sim", "sim", "sim"]}
+    pd.DataFrame.from_dict(data=clientes, orient='index').to_csv('clientes.csv', header=False)
     clientes_data_frame = pd.DataFrame(clientes)
+    
     negativados_clientes = clientes_data_frame.loc[clientes_data_frame["Saldo"] < 0]
     negativados_clientes.to_csv("negativados_clientes.csv")
     return negativados_clientes
@@ -30,8 +28,9 @@ lin()
 print(f"O(s) cliente(s) abaixo estão com saldo negativo - entrar em contato:")
 print(negativados)
 lin()
-print("Dados dos clientes com saldo negativo salvos em novo arquivo")
+print("Dados salvos em novo arquivo.")
 print("Verifique em seu diretório o arquivo 'negativados_clientes.csv'")
+print("Para ver a lista completa de clientes, pesquise 'clientes.csv'")
 print("Volte sempre!")
 lin()
 
